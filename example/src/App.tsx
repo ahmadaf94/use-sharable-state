43a0c424e05fb488aeae54f40c063cc8b5b3cb8c
Sam Afshari <sam@inkitt.com>
2022-08-09 11:43:52 +0430

    feat: complete the example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,46 +1,115 @@
-import { useSharableState } from 'use-sharable-state/lib/esm';
+import { useSharableState } from "use-sharable-state";
 
-function Child() {
-    const [count, setCount] = useSharableState('counter', 0)
-
-    return (
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-            <p>{count}</p>
-            <button type="button" onClick={() => setCount(count + 1)}>
-                increase
-            </button>
-        </div>
-    )
-}
-
-function AnotherChild() {
-    const [count, setCount] = useSharableState('counter', 0)
-
-    return (
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-            <p>{count}</p>
-            <button type="button" onClick={() => setCount(count + 1)}>
-                increase
-            </button>
-        </div>
-    )
-}
-
-function App() {
-  const [count, setCount] = useSharableState('counter', 0)
+const FirstUserCounter = () => {
+  const [count, setCount] = useSharableState("userCounter", 0);
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '1rem'}}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>User Counter #1</h1>
+
       <p>{count}</p>
+
       <button type="button" onClick={() => setCount(count + 1)}>
-        increase
+        Add User
       </button>
-
-        <Child />
-
-        <AnotherChild />
     </div>
-  )
-}
+  );
+};
 
-export default App
+const SecondUserCounter = () => {
+  const [count, setCount] = useSharableState("userCounter", 0);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>User Counter #2</h1>
+
+      <p>{count}</p>
+
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Add User
+      </button>
+    </div>
+  );
+};
+
+const FirstMessageCounter = () => {
+  const [count, setCount] = useSharableState("messageCounter", 0);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Message Counter #1</h1>
+
+      <p>{count}</p>
+
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Add Message
+      </button>
+    </div>
+  );
+};
+
+const SecondMessageCounter = () => {
+  const [count, setCount] = useSharableState("messageCounter", 0);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Message Counter #2</h1>
+
+      <p>{count}</p>
+
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Add Message
+      </button>
+    </div>
+  );
+};
+
+const App = () => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: "1rem",
+    }}
+  >
+    <FirstUserCounter />
+
+    <FirstMessageCounter />
+
+    <SecondUserCounter />
+
+    <SecondMessageCounter />
+  </div>
+);
+
+export default App;
